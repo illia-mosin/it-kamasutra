053abe5febf511eddc56c0f927c94c5fe2a50397
iMosin <imosin@MacBook-Pro-Macbook-2.local>
2020-12-03 13:34:12 +0200

    create NavLink, improving modules

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,18 +2,29 @@ import React from 'react';
 import s from './Nav.module.css';
 import Profile from "../Profile/Profile";
 import Friends from "../Friends/Friends";
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
-    <nav className={s.nav}>
-        <ul>
-            <li><a href="/Profile">Profile</a></li>
-            <li><a href="/Dialogs">Messages</a></li>
-            <li><a href="/Friends">friends</a></li>
-            <li><a href="/Tests">test</a></li>
-            <li><a href="#">settings</a></li>
-        </ul>
-    </nav>
+        <nav className={s.nav}>
+            <ul>
+                <li>
+                    <NavLink to="/Profile" activeClassName={s.activeLink}>Profile</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Dialogs" activeClassName={s.activeLink}>Messages</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Friends" activeClassName={s.activeLink}>Friends</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Tests" activeClassName={s.activeLink}>Test</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#" activeClassName={s.activeLink}>Settings</NavLink>
+                </li>
+            </ul>
+        </nav>
     );
 }
 export default Nav;
