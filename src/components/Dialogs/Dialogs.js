1b1d5513bfd9626c9274f761fd3d66d53c15a8ec
iMosin <imosin@MacBook-Pro-Macbook-2.local>
2020-12-07 14:48:01 +0200

    The simplest division into UI and BLL, hardcode post, message, dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -19,20 +19,33 @@ const Message = (props) => {
 
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Nastya'},
+        {id: 3, name: 'Stasyan'},
+        {id: 4, name: 'Misha'}
+    ]
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are your courses?'},
+        {id: 3, message: 'It-kamasutra best?'},
+        {id: 4, message: 'cool!)'}
+    ]
     return (
       <section>
           <div className={s.dialogs}>
               <div className={s.dialogsItems}>
-                  <DialogItem name="Dimych" id="1"/>
-                  <DialogItem name="Nastya" id="2"/>
-                  <DialogItem name="Stasyan" id="3"/>
-                  <DialogItem name="Misha" id="4"/>
+                  <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                  <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                  <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                  <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
 
               </div>
               <div className={s.messages}>
-                  <Message message="Hi"/>
-                  <Message message="How are you?)"/>
-                  <Message message="How is your IT-KAMASUTRA?)"/>
+                  <Message message={messagesData[0].message}/>
+                  <Message message={messagesData[1].message}/>
+                  <Message message={messagesData[2].message}/>
+                  <Message message={messagesData[3].message}/>
               </div>
           </div>
       </section>
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,6 +3,13 @@ import s from './MyPosts.module.css';
 import Post from "../../Post/Post";
 
 const MyPosts = () => {
+    let postsData = [
+        {id: 1, message: 'Hello, world!', likesCount: 20},
+        {id: 2, message: 'my second props on this project))', likesCount: 18},
+        {id: 3, message: 'it`s not so hard)', likesCount: 15},
+        {id: 4, message: 'fast learning react)', likesCount: 11},
+        {id: 5, message: 'Learning react)', likesCount: 11}
+    ]
     return (
         <main className={s.MyPosts}>
             <div className={s.postsBlock}>
@@ -10,10 +17,12 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message = "Hello, world!" likeCounter ="20"/>
-                <Post message = "my second props on this project))" likeCounter = "18"/>
-                <Post message = "it`s not so hard)" likeCounter = "15"/>
-                <Post message = "fast learning react)" likeCounter = "11"/>
+                <Post message = {postsData[0].message} likeCounter ={postsData[0].likesCount}/>
+                <Post message = {postsData[1].message} likeCounter ={postsData[1].likesCount}/>
+                <Post message = {postsData[2].message} likeCounter ={postsData[2].likesCount}/>
+                <Post message = {postsData[3].message} likeCounter ={postsData[3].likesCount}/>
+                <Post message = {postsData[4].message} likeCounter ={postsData[4].likesCount}/>
+
             </div>
 
         </main>
